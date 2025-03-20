3d27c218f9271143a79dd159400f5107d6232baa
hemadharshini819 <hemaj5781@gmail.com>
2025-03-20 06:58:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var body= document.querySelector("body");
+function dark(){
+ body.className="";
+  body.classList.add("dark");
+}
+function light(){
+ body.className="";
+ body.classList.add("light");
+}
